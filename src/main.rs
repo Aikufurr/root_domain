ff987bc659a46c047bcdaa8dc27df2fb0cd6659b
Aikufurr <ikeybill1@gmail.com>
2022-03-28 10:40:51 +0100

    Finished up the email part

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use rocket::fs::relative;
 
 #[get("/")]
 async fn index() -> Option<NamedFile> {
-    let path = Path::new("/drives/LinuxData0/aikufurr/Pictures/c2mec31oelg61.png");
+    let path = Path::new(relative!("/static/fox.png"));
     NamedFile::open(&path).await.ok()
 }
 
@@ -40,7 +40,7 @@ async fn presence(cmus_base: Option<&str>, cmus_artist: Option<&str>, cmus_album
         None => album = "".to_owned()
     }
 
-    let mut path: String = "/drives/LinuxData0/aikufurr/Pictures/c2mec31oelg61.png".to_owned();
+    let mut path: String = relative!("/static/fox.png").to_owned();
 
     if !artist.is_empty() && !album.is_empty() {
         if !base.is_empty() {
@@ -69,7 +69,7 @@ async fn presence(cmus_base: Option<&str>, cmus_artist: Option<&str>, cmus_album
 async fn wordle() -> Option<NamedFile> {
     let mut path = Path::new(relative!("static/wordle/wordle.html"));
     if !path.exists() {
-        path = Path::new("/drives/LinuxData0/aikufurr/Pictures/c2mec31oelg61.png");
+        path = Path::new(relative!("/static/fox.png"));
     }
     NamedFile::open(&path).await.ok()
 }
@@ -80,17 +80,32 @@ async fn not_found() -> Option<NamedFile> {
     NamedFile::open(&path).await.ok()
 }
 
-#[derive(Debug)]
+#[derive(Debug, FromForm, UriDisplayQuery)]
 struct EmailWebhook {
     recipient: String,
     subject: String,
 }
 
-fn get_details(id: &str) -> EmailWebhook {
+fn db_make_email(email: &EmailWebhook) -> String {
+    let conn = sqlite::open("database.db").unwrap();
+
+    let id: String = uuid::Uuid::new_v4().to_string().replace("-", "") + ".png";
+
+    let mut statement = conn.prepare("INSERT INTO emails (ID, RECIPIENT, SUBJECT) VALUES (?,?,?)").unwrap();
+    statement.bind(1, &*id).unwrap();
+    statement.bind(2, &*email.recipient).unwrap();
+    statement.bind(3, &*email.subject).unwrap();
+    
+    statement.next().unwrap();
+    
+    id
+}
+
+fn db_get_email(id: &str) -> EmailWebhook {
     let conn = sqlite::open("database.db").unwrap();
 
     let mut statement = conn.prepare("SELECT RECIPIENT, SUBJECT FROM emails WHERE ID = ?").unwrap();
-    statement.bind(1, id);
+    statement.bind(1, id).unwrap();
 
     let mut eml = EmailWebhook {
         recipient: "".to_owned(),
@@ -107,28 +122,64 @@ fn get_details(id: &str) -> EmailWebhook {
     eml
 }
 
+fn db_del_email(id: &str) {
+    let conn = sqlite::open("database.db").unwrap();
+
+    let mut statement = conn.prepare("DELETE FROM emails WHERE ID = ?").unwrap();
+    statement.bind(1, id).unwrap();
+    statement.next().unwrap();
+}
+
+#[get("/email?<email..>")]
+async fn email_make(email: EmailWebhook) -> String {
+
+    db_make_email(&email)
+}
+
 #[get("/email/<id>")]
 async fn email(id: String) -> Option<NamedFile> {
+    let details = db_get_email(&id);
+
     let mut map = HashMap::new();
-
-    // let results = tokio::task::block_in_place(move || -> std::result::Result<rusqlite::MappedRows<()>, ()> {
-        
-    // });
-
-    let details = get_details(&id);
-
     map.insert("content", "<@308681202548604938>, your email to `".to_owned() + &details.recipient + "` about `" + &details.subject + "` was read!");
 
     let client = reqwest::Client::new();
         .json(&map)
         .send()
-        .await.ok()?;
+        .await;
 
-    let path = Path::new("/drives/LinuxData0/aikufurr/Pictures/c2mec31oelg61.png").to_path_buf();
+    match response {
+        Ok(_) => {},
+        Err(_) => {}
+    };
+
+    db_del_email(&id);
+
+    let path = Path::new(relative!("/static/1x1.png")).to_path_buf();
     NamedFile::open(&path).await.ok()
+}
+
+fn init_db() {
+    let conn = sqlite::open("database.db").unwrap();
+
+    let mut statement = conn.prepare("SELECT count(name) FROM sqlite_master WHERE type='table' AND name = ?").unwrap();
+    statement.bind(1, "emails").unwrap();
+    while let sqlite::State::Row = statement.next().unwrap() {
+        if statement.read::<i64>(0).unwrap() == 0 {
+            statement = conn.prepare(r#"CREATE TABLE "emails" (
+                "ID"	TEXT NOT NULL UNIQUE,
+                "RECIPIENT"	TEXT,
+                "SUBJECT"	TEXT,
+                PRIMARY KEY("ID")
+            )"#).unwrap();
+            statement.next().unwrap();
+        }
+        break;
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, file, presence, wordle, email]).register("/", catchers![not_found])
+    init_db();
+    rocket::build().mount("/", routes![index, file, presence, wordle, email_make, email]).register("/", catchers![not_found])
 }
